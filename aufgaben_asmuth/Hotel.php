f2fa461aff3436b65f08293dbb45f3ded54133c4
Namydad <tobias.lammer.tl31@gmail.com>
2023-11-09 14:51:53 +0100

    Übungsaufgabe Co-Pilot

---
+++ aufgaben_asmuth/Hotel.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cinema Copilot</title>
+    /**
+     * This code block includes links to Bootstrap CSS and JS files for styling and functionality purposes.
+     * Bootstrap is a popular front-end framework that provides a set of pre-built CSS and JavaScript components for building responsive, mobile-first websites and web applications.
+     * The CSS file provides styling for various HTML elements, while the JavaScript files provide functionality for interactive components such as dropdown menus, modals, and carousels.
+     * By including these files in your project, you can take advantage of Bootstrap's pre-built components to quickly and easily create a professional-looking website or application.
+     */
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <!-- Header -->
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center my-5">Cinema Copilot</h1>
+            </div>
+        </div>
+        <!-- Table -->
+        <div class="row">
+            <div class="col">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Theatre</th>
+                            <th>Name</th>
+                            <th>Runtime</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>The Shawshank Redemption</td>
+                            <td>2h 22min</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>The Godfather</td>
+                            <td>2h 55min</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>The Dark Knight</td>
+                            <td>2h 32min</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
